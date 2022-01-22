72fdf7650765211d2e0c8a5006b7c9dbdeee29e0
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-01-22 23:52:13 +0100

    module (JAXB): Fixing dependencies ...

--- JAXB-tools-and-plugins/src/main/generated/com/cleverdeveloper/patient/Patient.java
+++ JAXB-tools-and-plugins/src/main/generated/com/cleverdeveloper/patient/Patient.java
@@ -8,17 +8,11 @@
 
 package com.cleverdeveloper.patient;
 
+import javax.xml.bind.annotation.*;
+import javax.xml.datatype.XMLGregorianCalendar;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlSchemaType;
-import javax.xml.bind.annotation.XmlType;
-import javax.xml.datatype.XMLGregorianCalendar;
 
 
 /**
