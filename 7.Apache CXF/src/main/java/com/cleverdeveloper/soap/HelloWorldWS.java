31b8ffd49fd4dac5cfa745c3763994aac7f3966a
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-01-27 15:39:11 +0100

    7. Apache CXF: Fixing dependencies & Running the application ...

--- 7.Apache CXF/src/main/java/com/cleverdeveloper/soap/HelloWorldWS.java
+++ 7.Apache CXF/src/main/java/com/cleverdeveloper/soap/HelloWorldWS.java
@@ -1,18 +1,18 @@
-package com.cleverdeveloper.ws.soap;
+package com.cleverdeveloper.soap;
 
 /*
 PROJECT NAME : Java-Web-Services-Part-01
 Module NAME: IntelliJ IDEA
 Author Name : @ DRRONIDZ
-DATE : 27/01/2022 14:22
+DATE : 27/01/2022 15:13
 */
+
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
-/**/
 @WebService
-public class HelloWordWebService {
+public class HelloWorldWS {
 
     @WebMethod
     public String helloWorld() {
--- 7.Apache CXF/src/main/java/com/cleverdeveloper/soap/config/WebServiceConfig.java
+++ 7.Apache CXF/src/main/java/com/cleverdeveloper/soap/config/WebServiceConfig.java
@@ -1,16 +1,17 @@
-package com.cleverdeveloper.ws.soap.config;
+package com.cleverdeveloper.soap.config;
 
 /*
 PROJECT NAME : Java-Web-Services-Part-01
 Module NAME: IntelliJ IDEA
 Author Name : @ DRRONIDZ
-DATE : 27/01/2022 14:37
+DATE : 27/01/2022 15:10
 */
 
-import com.cleverdeveloper.ws.soap.HelloWordWebService;
+import com.cleverdeveloper.soap.HelloWorldWS;
 import org.apache.cxf.Bus;
 import org.apache.cxf.jaxws.EndpointImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,12 +20,12 @@ import javax.xml.ws.Endpoint;
 @Configuration
 public class WebServiceConfig {
 
-
+    @Autowired
     private Bus bus;
 
     @Bean
     public Endpoint endpoint() {
-        Endpoint endpoint = new EndpointImpl(bus, new HelloWordWebService());
+        Endpoint endpoint = new EndpointImpl(bus, new HelloWorldWS());
         endpoint.publish("/hello");
         return endpoint;
     }
