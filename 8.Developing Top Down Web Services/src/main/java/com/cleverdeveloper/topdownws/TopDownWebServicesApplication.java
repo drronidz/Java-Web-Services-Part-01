cc266b1f8150906db2847ca0994f80beaba4c16b
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-01-27 21:53:02 +0100

    8. Developing Top Down Web Services: Create the WSDL First project ...

---
+++ 8.Developing Top Down Web Services/src/main/java/com/cleverdeveloper/topdownws/TopDownWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.cleverdeveloper.topdownws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopDownWebServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopDownWebServicesApplication.class, args);
+    }
+
+}
---
+++ 8.Developing Top Down Web Services/src/test/java/com/cleverdeveloper/topdownws/TopDownWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleverdeveloper.topdownws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopDownWebServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
