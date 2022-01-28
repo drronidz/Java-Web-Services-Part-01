01e8d15254fa236467718b8c9ed9028cee66ffbd
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-01-28 14:08:42 +0100

    8. Developing Top Down Web Services: Implementing the method init()

--- 8.Developing Top Down Web Services/src/main/java/com/cleverdeveloper/topdownws/CustomerOrdersWSImpl.java
+++ 8.Developing Top Down Web Services/src/main/java/com/cleverdeveloper/topdownws/CustomerOrdersWSImpl.java
@@ -9,11 +9,37 @@ DATE : 27/01/2022 22:49
 
 
 import cleverdeveloper.trainings.*;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /*
 * Dependencies to be fixed in generated sources folder ...
 * */
 public class CustomerOrdersWSImpl implements CustomerOrdersPortType {
 
+    Map<BigInteger, List<Order>> customerOrders = new HashMap<>();
+    int currentId;
+
+    public void init() {
+        List<Order> orders = new ArrayList<>();
+        Order order = new Order();
+        order.setId(BigInteger.valueOf(1));
+
+        Product product = new Product();
+        product.setId(String.valueOf(1));
+        product.setDescription("ThinkPad x260");
+        product.setQuantity(BigInteger.valueOf(1));
+
+        order.getProduct().add(product);
+
+        orders.add(order);
+
+        customerOrders.put(BigInteger.valueOf(++currentId), orders);
+    }
 
     @Override
     public GetOrdersResponse getOrders(GetOrdersRequest parameters) {
