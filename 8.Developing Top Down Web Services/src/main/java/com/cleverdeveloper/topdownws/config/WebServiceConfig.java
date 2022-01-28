3ab9c94ae3631227e8be161684bf6fb207c74287
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-01-28 14:22:27 +0100

    8. Developing Top Down Web Services: Publish the endpoint (by creating the configuration class) ...

---
+++ 8.Developing Top Down Web Services/src/main/java/com/cleverdeveloper/topdownws/config/WebServiceConfig.java
@@ -0,0 +1,32 @@
+package com.cleverdeveloper.topdownws.config;
+
+/*
+PROJECT NAME : Java-Web-Services-Part-01
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 27/01/2022 15:10
+*/
+
+
+import com.cleverdeveloper.topdownws.CustomerOrdersWSImpl;
+import org.apache.cxf.Bus;
+import org.apache.cxf.jaxws.EndpointImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.xml.ws.Endpoint;
+
+@Configuration
+public class WebServiceConfig {
+
+    @Autowired
+    private Bus bus;
+
+    @Bean
+    public Endpoint endpoint() {
+        Endpoint endpoint = new EndpointImpl(bus, new CustomerOrdersWSImpl());
+        endpoint.publish("/customerordersservice");
+        return endpoint;
+    }
+}
