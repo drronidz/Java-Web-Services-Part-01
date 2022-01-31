af6b2928255689134f3a19c30f3e4f0c91890782
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-01-31 13:22:57 +0100

    10. Developing Bottom Up Web Services 4. Mark the Endpoint with JAX-WS Annotations

--- 10. Developing Bottom Up Web Services/src/main/java/com/cleverdeveloper/javafirstws/soap/PaymentProcessor.java
+++ 10. Developing Bottom Up Web Services/src/main/java/com/cleverdeveloper/javafirstws/soap/PaymentProcessor.java
@@ -1,9 +1,22 @@
 package com.cleverdeveloper.javafirstws.soap;
 
-import com.bharath.ws.soap.dto.PaymentProcessorRequest;
-import com.bharath.ws.soap.dto.PaymentProcessorResponse;
 
+import com.cleverdeveloper.javafirstws.dto.PaymentProcessorRequest;
+import com.cleverdeveloper.javafirstws.dto.PaymentProcessorResponse;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+
+/* @Annotation (@WebService)[required] : */
+/* @ tells apache CXF or any SOAP run time that this particular class or interface should be exposed out as a web service*/
+/* @ that's the only annotation that is required to expose this as a web service (the key annotation)*/
+
+@WebService(name= "PaymentProcessor")
 public interface PaymentProcessor {
-
-	public PaymentProcessorResponse processPayment(PaymentProcessorRequest paymentProcessorRequest);
+	/*@ @Annotation (@webMethod)[optional] :*/
+	/*@ This method will be exposed out automatically as web service method.*/
+	@WebMethod
+	public @WebResult(name = "response") PaymentProcessorResponse processPayment(@WebParam(name = "paymentProcessorRequest") PaymentProcessorRequest paymentProcessorRequest);
 }
